08e6cad5912125c8bab18c8bfb7163313f090b59
nate-droid <nathaniel.ham@upcloud.com>
2025-07-29 20:39:57 +0300

    fixed a bunch of react routes

--- wasm-ui/ui/app/routes.ts
+++ wasm-ui/ui/app/routes.ts
@@ -2,10 +2,10 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  // route("visualizer", "routes/visualizer.tsx"),
-  // route("svm", "routes/svm.tsx"),
-  // route("qcbm", "routes/qcbm.tsx"),
+  route("visualizer", "routes/visualizer.tsx"),
+  route("svm", "routes/svm.tsx"),
+  route("qcbm", "routes/qcbm.tsx"),
   route("qasm", "routes/qasm.tsx"),
-  // route("ml-svm", "routes/ml-svm.tsx"),
+  route("ml-svm", "routes/ml-svm.tsx"),
   route("simulator", "routes/simulator.tsx"),
 ] satisfies RouteConfig;
